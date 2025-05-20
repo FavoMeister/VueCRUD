1b42c92105e60562c8eca24ccb9ddf463aa7bf21
FavoMeister <93888759+FavoMeister@users.noreply.github.com>
2025-05-20 00:29:08 -0400

    Init Project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>Acerca de nosotros</h1>
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <h1>Inicio</h1>
+  </main>
+</template>
